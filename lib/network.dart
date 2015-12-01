c6ec0e25ef2c292baf85d3d9ab71cb8679a4cc61
Robert McDermot <robert.mcdermot@gmail.com>
2015-11-30 23:19:57 -0500

    works with one person (client)

--- lib/network.dart
+++ lib/network.dart
@@ -1,66 +1,74 @@
 import 'dart:io';
 
 // Configuration
-int     GAME_SERVER_PORT = 25565;
-String  WEB_ADDRESS = 'localhost';
+int GAME_SERVER_PORT = 25565;
+String WEB_ADDRESS = 'localhost';
 
 
 class Host {
-  Socket game;
-  WebSocket ws;
+    Socket game;
+    WebSocket ws;
 
-  refresh() async {
-    //setup websocket
-    ws = await WebSocket.connect('ws://$WEB_ADDRESS:8080');
-    print('websocket connected');
+    refresh() async {
+        //setup websocket
+        ws = await WebSocket.connect('ws://$WEB_ADDRESS:8080');
+        print('websocket connected');
 
-    // game server
-    game = await Socket.connect('0.0.0.0', GAME_SERVER_PORT);
-    print('game server connected');
+        // game server
+        game = await Socket.connect('0.0.0.0', GAME_SERVER_PORT);
+        print('game server connected');
 
-    // sync
-    ws.listen((data) {
-      print('websocket: ' + new String.fromCharCodes(data));
-      game.add(data);
-    });
-    game.listen((data) {
-      print('game: ' +new String.fromCharCodes(data));
-      ws.add(data);
-    });
-  }
+        // sync
+        ws.listen((data) {
+            print('websocket: ' + new String.fromCharCodes(data));
+            game.add(data);
+        });
+        game.listen((data) {
+            print('game: ' + new String.fromCharCodes(data));
+            ws.add(data);
+        });
+    }
 }
 
 class Server {
+    List<Socket> clients = [];
 
-  refresh() async {
-    // websockets
-    HttpServer http = await HttpServer.bind('0.0.0.0', 8080);
-    http.listen((request) async {
-      WebSocket wsServer = await WebSocketTransformer.upgrade(request);
+    refresh() async {
+        // websockets
+        ServerSocket http = await ServerSocket.bind('0.0.0.0', 8080);
+        print('bound to 8080');
+        http.listen((Socket s) async {
+            ServerSocket clientServer = await ServerSocket.bind('0.0.0.0', 8081);
+            print('bound to 8081');
 
-      http = await HttpServer.bind('0.0.0.0', 8081);
-      request = await http.first;
-      http.listen((request) async {
-        WebSocket wsClient = await WebSocketTransformer.upgrade(request);
+            clientServer.listen((Socket socket) async {
+                print('got a client connection');
+                clients.add(socket);
+                socket.listen((data) => s.add(data));
+            });
 
-        wsClient.listen((data) => wsServer.add(data));
-        wsServer.listen((data) => wsClient.add(data));
-      });
+            s.listen((data) => passToClient(data));
+        });
+    }
 
-    });
-  }
+    passToClient(data) {
+        clients.forEach((Socket wsClient) {
+            wsClient.add(data);
+        });
+    }
 }
 
 class Client {
-  refresh() async {
-    // game client
-    ServerSocket game = await ServerSocket.bind('0.0.0.0', GAME_SERVER_PORT+1);
-    game.listen((socket) async {
-      WebSocket ws = await WebSocket.connect('ws://$WEB_ADDRESS:8081');
-      socket.listen((data) => ws.add(data));
-      ws.listen((data) {
-        socket.add(data);
-      });
-    });
-  }
+    refresh() async {
+        // game client
+        ServerSocket game = await ServerSocket.bind('0.0.0.0', GAME_SERVER_PORT + 1);
+        print('bound to ${GAME_SERVER_PORT+1}');
+        game.listen((socket) async {
+            Socket ws = await Socket.connect(WEB_ADDRESS, 8081);
+            socket.listen((data) => ws.add(data));
+            ws.listen((data) {
+                socket.add(data);
+            });
+        });
+    }
 }
